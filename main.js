1a6ba987c2df5796800aecda43b9593db74a25ed
ccaps <christophcaps@gmail.com>
2016-10-25 14:56:52 +0200

    Fenstergröße angepasst

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1024, height: 768})
+  win = new BrowserWindow({width: 300, height: 768})
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
